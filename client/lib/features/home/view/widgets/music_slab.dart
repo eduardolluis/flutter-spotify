83aed96b526a371c0fe513c31ddb3719ff4eb453
eduardolluis <1132907@est.intec.edu.do>
2026-07-19 17:16:04 -0400

    music slab UI

---
+++ client/lib/features/home/view/widgets/music_slab.dart
@@ -0,0 +1,104 @@
+import 'package:client/core/providers/current_song_notifier.dart';
+import 'package:client/core/theme/app_pallete.dart';
+import 'package:client/core/utils.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MusicSlab extends ConsumerWidget {
+  const MusicSlab({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentSong = ref.watch(currentSongProvider);
+    if (currentSong == null) {
+      return const SizedBox();
+    }
+    return Stack(
+      children: [
+        Container(
+          height: 66,
+          width: MediaQuery.of(context).size.width - 16,
+          decoration: BoxDecoration(
+            color: hexToColor(currentSong.hex_code),
+            borderRadius: BorderRadius.circular(4),
+          ),
+          padding: const EdgeInsets.all(9),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 48,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                      image: DecorationImage(
+                        image: NetworkImage(currentSong.thumbnail_url),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        currentSong.song_name,
+                        style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+                      ),
+                      Text(
+                        currentSong.artist,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14,
+                          color: Pallete.subtitleText,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(CupertinoIcons.heart, color: Pallete.whiteColor),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(CupertinoIcons.play_fill, color: Pallete.whiteColor),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          child: Container(
+            height: 2,
+            width: 20,
+            decoration: BoxDecoration(
+              color: Pallete.whiteColor,
+              borderRadius: BorderRadius.circular(7),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 8,
+          child: Container(
+            height: 2,
+            width: MediaQuery.of(context).size.width - 32,
+            decoration: BoxDecoration(
+              color: Pallete.inactiveSeekColor,
+              borderRadius: BorderRadius.circular(7),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
